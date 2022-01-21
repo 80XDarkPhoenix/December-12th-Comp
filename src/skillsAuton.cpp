cc3dea16c66a7e40787994f9ce15a0345985d6ec
80XDarkPhoenix <darkphoenixvexmen@gmail.com>
2022-01-20 23:28:07 -0500

    code fpr kalahari

--- src/skillsAuton.cpp
+++ src/skillsAuton.cpp
@@ -51,26 +51,26 @@ void skillsAuton() {
   delay(50);
   turn(-45,100);
   delay(50);
-  move(5,120);
+  move(10,120);
   moveLift(-127);
   delay(200);
   moveLift(0);
   unhookFrontClaw();
-  move(-8,120);
+  move(-9,120);
   delay(50);
-  turn(-93,110);
+  turn(-95,110);
   delay(50);
   unhookBackClaw();
   delay(200);
   moveLift(-127);
-  move(45,120);
+  move(52,120);
   moveLift(0);
   hookFrontClaw();
   delay(250);
-  move(-16,110);
+  move(-20,110);
   turn(62,100);
   delay(50);
-  move(-40,120);
+  move(-100,120);
 
 
 //  turn(-45, 110);
